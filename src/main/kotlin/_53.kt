9f187c48e4e151c5d0fc9b8b847695a3a7039056
Dmitry Valov <d_valov@mail.ru>
2021-04-30 06:35:18 +0600

    Added solved problem - Maximum Subarray

--- src/main/kotlin/_53.kt
+++ src/main/kotlin/_53.kt
@@ -5,14 +5,12 @@
 class _53 {
     class Solution {
         fun maxSubArray(nums: IntArray): Int {
-            val dp = nums.clone()
-            var maxSum = nums.first()
-            dp[0] = nums.first()
+            var sum = nums.first()
             for (i in 1 until nums.size) {
-                dp[i] = kotlin.math.max(dp[i - 1] + nums[i], nums[i])
-                maxSum = kotlin.math.max(dp[i], maxSum)
+                nums[i] = kotlin.math.max(nums[i - 1] + nums[i], nums[i])
+                sum = kotlin.math.max(nums[i], sum)
             }
-            return maxSum
+            return sum
         }
     }
 }
